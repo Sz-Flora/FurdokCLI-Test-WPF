2a0d8d06743645c256f0eaf2161ee86f12c37f67
Sz-Flora <szabof@kkszki.hu>
2026-05-19 12:00:16 +0200

    Add files via upload

---
+++ cli/Furdo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrandCLI
+{
+    public class Furdo
+    {
+        public string Nev { get; private set; }
+        public string Cim { get; private set; }
+        public int Ar { get; private set; }
+        public int Vizhofok { get; private set; }
+
+        public Furdo(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Nev = adatok[0];
+            this.Cim = adatok[1];
+            this.Ar = int.Parse(adatok[2]);
+            this.Vizhofok = int.Parse(adatok[3]);
+        }
+
+        public string IRSZ()
+        {
+            string[] cim = Cim.Split(" ");
+            return cim[0];
+        }
+
+        public string Telepules()
+        {
+            string[] cim = Cim.Split(" ");
+            return cim[1].Replace(",", "");
+        }
+    }
+}
---
+++ cli/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace StrandCLI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Furdo> furdok = new List<Furdo>();
+            var sorok = File.ReadAllLines("strandadatok.txt").Skip(1);
+
+            foreach (var sor in sorok)
+            {
+                furdok.Add(new Furdo(sor));
+            }
+
+            Console.WriteLine("7. feladat:");
+            Console.WriteLine($"Fürdők száma: {furdok.Count}");
+
+            Console.WriteLine("8. feladat:");
+            int osszeg = 0;
+            foreach (var f in furdok)
+            {
+                osszeg += f.Ar;
+            }
+            double atlag = (double)osszeg / furdok.Count;
+            Console.WriteLine($"A fürdőbelépők átlagos ára: {atlag:F1}");
+
+            Console.WriteLine("9. feladat:");
+            Furdo leghidegebb = furdok.MinBy(x => x.Vizhofok);
+            Console.WriteLine($"A leghidegebb víz a(z) {leghidegebb.Cim} nevű fürdőben van.");
+
+            Console.WriteLine("10. feladat:");
+            Console.WriteLine("Kérem, adja meg egy fürdő nevét!");
+            string bekertfurdo = Console.ReadLine();
+            bool vane = false;
+            foreach (var f in furdok)
+            {
+                if (f.Nev == bekertfurdo)
+                {
+                    vane = true;
+                    Console.WriteLine($"A fürdő {f.Telepules()} településen van, az írányítószáma: {f.IRSZ()}");
+                    break;
+                }
+            }
+            if (vane == false)
+            {
+                Console.WriteLine("Nincs ilyen nevű fürdő.");
+            }
+
+        }
+    }
+}
